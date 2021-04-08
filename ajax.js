ca7001a76f8e3ec15cd9f69d7bfaecc54fd775ee
aysha <ayshag611@gmail.com>
2021-04-08 02:49:19 +0200

    .

--- ajax.js
+++ ajax.js
@@ -1,4 +1,164 @@
 //console.log('correcto');
+document.querySelector('#restaurants').addEventListener('click',traerDatos)
+
+mostrarRestaurants();
+
+function mostrarRestaurants(){
+    const xhttp = new XMLHttpRequest()
+    // el true indica que es asincrono, aqui importamos los datos 
+    xhttp.open('GET', 'Restaurants.json', true)
+
+    xhttp.send()
+
+    xhttp.onreadystatechange = function(){
+        let datos = JSON.parse(xhttp.responseText)
+
+        //tenemos todos los restaurantes
+
+        if(this.readyState==4 && this.status==200){ //esto sale en otro video y parece que siempre es asi
+            let restaurants = document.querySelector('#restaurants')
+            restaurants.innerHTML = ''
+          //  restaurants.innerHTML += `<div class="row">`
+            for(let item of datos){
+                restaurants.innerHTML += `
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" id="restaurants" href="#plantillaFlotante"> 
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src=${item.imatges[0]} alt="" />
+                            </a>
+
+
+                            <div class="portfolio-modal modal fade" id="plantillaFlotante" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                                        <div class="container">
+                                            <div class="row justify-content-center">
+                                                <div class="modal-body">
+                                                    <h2 class="text-uppercase">${item.nom}</h2>
+                                                    <!-- aquí poner lo que viene dentro de la página flotante -->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">${item.nom}</div>
+                                <div class="portfolio-caption-subheading text-muted">${item.geo1.address}</div>
+                            </div>
+                        </div>
+                    </div>  
+                `
+            }
+           // restaurants.innerHTML += `</div> `
+
+
+
+
+
+
+            /*
+            let geo1 = document.querySelector('#geo1')
+            geo1.innerHTML = datos[0].geo1.address
+            
+            let nombreRestaurante = document.querySelector('#nombreRestaurante')
+            nombreRestaurante.innerHTML = datos[0].nom
+            console.log(nombreRestaurante)
+*/
+            
+        }
+    }
+}
+
+
+/*
+function traerDatosBasicos(){
+    const xhttp = new XMLHttpRequest();
+
+    // el true indica que es asincrono, aqui importamos los datos 
+    xhttp.open('GET', 'Restaurants.json', true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){ //esto sale en otro video y parece que siempre es asi
+         
+            let geo1 = document.querySelector('#geo1')
+            geo1.innerHTML = datos[0].geo1.address
+            
+            let nombreRestaurante = document.querySelector('#nombreRestaurante')
+            nombreRestaurante.innerHTML = datos[0].nom
+        }
+    }
+}
+
+*/
+//función que trae los datos cuando se pulsa el boton
+function traerDatos(){
+   // console.log('dentro de la función');
+   console.log('estamos en la función traer datos')
+   
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){ //esto sale en otro video y parece que siempre es asi
+            
+
+            
+            let geo1 = document.querySelector('#geo1')
+            geo1.innerHTML = datos[0].geo1.address
+
+            let horari = document.querySelector('#horari')
+            horari.innerHTML += `
+                <p>Horari:</p>
+                <ul>
+                    <li>Dilluns: ${datos[0].horari.di[0].in}-${datos[0].horari.di[0].out}</li>
+                    <li>Dimarts: ${datos[0].horari.dm[0].in}-${datos[0].horari.document[0].out}</li>
+                    <li>Dimecres: ${datos[0].horari.dx[0].in}-${datos[0].horari.dx[0].out}</li>
+                    <li>Dijous: ${datos[0].horari.dj[0].in}-${datos[0].horari.dj[0].out}</li>
+                    <li>Divendres: ${datos[0].horari.dv[0].in}-${datos[0].horari.dv[0].out}</li>
+                    <li>Dissabte: ${datos[0].horari.ds[0].in}-${datos[0].horari.ds[0].out}</li>
+                    <li>Diumenge: ${datos[0].horari.dg[0].in}-${datos[0].horari.dg[0].out}</li>
+                </ul>
+                `
+            let descripcio = document.querySelector('#descripcio')
+            descripcio.innerHTML = datos[0].descripcio
+
+            let telefono = document.querySelector('#telefono')
+            telefono.innerHTML = datos[0].contacte.telf
+
+
+
+            let nombreRestaurante = document.querySelector('#nombreRestaurante')
+            nombreRestaurante.innerHTML = datos[0].nom
+            console.log('se ha guardado el valor de nombrerestaurante');
+
+/*
+            let carouselRestaurant = document.querySelector('#carouselRestaurant')
+
+            for(let fotos in datos[1].imatges){
+                carouselRestaurant += ``
+                
+            }
+            respuesta.innerHTML += `
+                    
+                        <p>${item.nom}</p>
+                        <p>${item.descripcio}</p>
+                        <p>${item.tipus}</p>
+                        <p>${item.puntuacio}</p>
+                `*/
+        }
+    }
+
+}
+
+
+
+/*//console.log('correcto');
 
 mostrarRestaurants();
 document.querySelector('#restaurants').addEventListener('click', traerDatos)
@@ -69,7 +229,7 @@ function traerDatos() {
                 </div>
             `*/
 
-
+/*
 
 
             let geo1 = document.querySelector('#geo1')
