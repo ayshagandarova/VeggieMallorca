5359a06a119c3878911430e093d8035bd3beb0e4
Maria Orell Monserrat <mariaorell2000@gmail.com>
2021-05-09 13:27:17 +0200

    horario

--- js/portfolio.js
+++ js/portfolio.js
@@ -530,6 +530,26 @@ function desplegable(i) {
 }
 
 
+//Funcion del desplegable de horario
+ /* When the user clicks on the button, 
+ toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
 
 //+ INFORMACIO.HTML
