6e5de0a2dfee21ced37355e461c8c5a56dd06806
aysha <ayshag611@gmail.com>
2021-05-24 14:03:01 +0200

    quitado algunos comentarios

--- js/portfolio.js
+++ js/portfolio.js
@@ -66,69 +66,6 @@ inputBox.addEventListener('keyup', (e) => {
 });
 
 /*
-    let webLink;
-    var xmlhttp = new XMLHttpRequest();
-    var url = "dades.json";
-    xmlhttp.onreadystatechange = function () {
-        
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var suggestions = JSON.parse(xmlhttp.responseText);
-            inputBox.onkeyup = (e)=>{
-                let userData = e.target.value; //user enetered data
-                let emptyArray = [];
-                
-                if(userData){
-                    icon.onclick = ()=>{
-                        webLink = "https://www.google.com/search?q=" + userData;
-                        linkTag.setAttribute("href", webLink);
-                        console.log(webLink);
-                        linkTag.click();
-                    }
-                    
-                    emptyArray = suggestions.filter((data)=>{
-                        //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-                        return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
-                    });
-                    emptyArray = emptyArray.map((data)=>{
-                        // passing return data inside li tag
-                        return data = '<li>'+ data +'</li>';
-                    });
-                    searchWrapper.classList.add("active"); //show autocomplete box
-                    showSuggestions(emptyArray);
-                    let allList = suggBox.querySelectorAll("li");
-                    for (let i = 0; i < allList.length; i++) {
-                        //adding onclick attribute in all li tag
-                        allList[i].setAttribute("onclick", "select(this)");
-                    }
-                }else{
-                    searchWrapper.classList.remove("active"); //hide autocomplete box
-                }
-            }
-        }
-    }
-});
-            
-function select(element){
-    let selectData = element.textContent;
-    inputBox.value = selectData;
-    icon.onclick = ()=>{
-        webLink = "https://www.google.com/search?q=" + selectData;
-        linkTag.setAttribute("href", webLink);
-        linkTag.click();
-    }
-    searchWrapper.classList.remove("active");
-}
-             
-function showSuggestions(list){
-    let listData;
-    if(!list.length){
-        userValue = inputBox.value;
-        listData = '<li>'+ userValue +'</li>';
-    }else{
-        listData = list.join('');
-    }
-    suggBox.innerHTML = listData;
-}
 
 //function searchBar() {
 // coger el texto y imprimir los elementos en la consola que se corresponden a la busqueda 
