7d19574bb3b6c1825e74d41a9f72d1c3971253ee
Maria Orell Monserrat <mariaorell2000@gmail.com>
2021-05-30 18:55:49 +0200

    intento de semantica index.html

--- js/index.js
+++ js/index.js
@@ -1,34 +1,109 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
+//Web Semantica
+var info = generarJsonLDIndex();
+loadJSON_LD(info);
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+/**
+ * Funció per afegir el JSON-LD a la pàgina web
+ */
+function loadJSON_LD(info) {
+  const script = document.createElement('script');
+  script.setAttribute('type', 'application/ld+json');
+  script.textContent = JSON.stringify(info);
+  document.head.appendChild(script);
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+//crea el jsonld para el indice
+function carregarJsonldIndex() {
+  //let infoFiraPortada = crearJsonldFira(firaPortada);
+  let info = {
+    "@context": "http://schema.org/",
+    "@type": "WebApplication",
+    "applicationCategory": "Restaurantes Vegetarianos Mallorca, Supermercados Vegetarianos Mallorca, Información Vegetarianos Mallorca",
+    "applicationSubCategory": "Cursos, Ferias",
+    "about": "Información vegetariana Mallorca",
+    "name": "Veggie Mallorca",
+    "description": "MENJAR BÉ TOTS ELS DIES ÉS MÉS SENZILL DEL QUE ET PENSES. Descobreix les meravelles que ofereix el món vegetarià: troba aquí tota la inspiració necessària i troba una nova forma de cuidar-te.",
+    "audience": {
+      "@type": "Audience",
+      "audienceType": "familia, turistas, locales, grupos, amigos",
+      "geographicArea": "Mallorca"
+    },
+    "author": [
+      {
+        "@type": "Person",
+        "givenName": "Aisha",
+        "familyName": "Gandarova",
+        "gender": "Female",
+      },
+      {
+        "@type": "Person",
+        "givenName": "Maria",
+        "familyName": "Orell",
+        "gender": "Female",
+      }
+    ],
+    "contentLocation":
+    {
+      "address": "Mallorca, Islas Baleares, Spain",
+      "geo":
+      {
+        "@type": "GeoCoordinates",
+        "latitude": "39.69988587628037",
+        "longitude": "3.016459872869967"
+      }
+    },
+
+    "genre": "Restaurants Vegetarians Mallorca, Supermercats Vegetarians Mallorca",
+    "video":
+    {
+      "@type": "VideoObject",
+      "name": "Vídeo Cocina",
+      "description": "Video de cocina vegetariana Mallorca",
+      "contentUrl": ["assets/img/fotosIndex/videoPortada.mp4", "assets/img/fotosIndex/videoPortada.webm"],
+      "about": {
+        "@type": "Food"
+      },
+      "uploadDate": "2021-05-15",
+    }
+  }
+  return info;
+  //loadJSON_LD(info);
+}
+
+
+
+
+  //Get the button
+  var mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+  }
 
-// Animación con scroll:
+  // Animación con scroll:
 
-// Detect request animation frame
-var scroll = window.requestAnimationFrame ||
-             // IE Fallback
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+  // Detect request animation frame
+  var scroll = window.requestAnimationFrame ||
+    // IE Fallback
+    function (callback) { window.setTimeout(callback, 1000 / 60) };
+  var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
-function loop() {
+  function loop() {
 
-    Array.prototype.forEach.call(elementsToShow, function(element){
+    Array.prototype.forEach.call(elementsToShow, function (element) {
       if (isElementInViewport(element)) {
         element.classList.add('is-visible');
       } else {
@@ -37,122 +112,122 @@ function loop() {
     });
 
     scroll(loop);
-}
-
-// Call the loop for the first time
-loop();
-
-// Helper function from: http://stackoverflow.com/a/7557433/274826
-function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-    
   }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
 
-/* MAPA */
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXNob29rMDA3IiwiYSI6ImNrbnZ4bGg3bzByMTcydnFucWdpcGx6bWEifQ.jHKo86UYDX6fcEVz_VoHZQ';
-var map = new mapboxgl.Map({
+  // Call the loop for the first time
+  loop();
+
+  // Helper function from: http://stackoverflow.com/a/7557433/274826
+  function isElementInViewport(el) {
+    // special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
+
+  /* MAPA */
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYXNob29rMDA3IiwiYSI6ImNrbnZ4bGg3bzByMTcydnFucWdpcGx6bWEifQ.jHKo86UYDX6fcEVz_VoHZQ';
+  var map = new mapboxgl.Map({
     container: 'mapidGran', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [2.942477242579887, 39.63623814828035], // starting position [lng, lat]
     zoom: 8.8 // starting zoom
-});
-map.touchZoomRotate.enable();
-map.on('idle', function () {
+  });
+  map.touchZoomRotate.enable();
+  map.on('idle', function () {
     map.resize()
-});
-map.scrollZoom.disable();
-map.addControl(new mapboxgl.NavigationControl());
-// Add geolocate control to the map.
-map.addControl(new mapboxgl.GeolocateControl({
+  });
+  map.scrollZoom.disable();
+  map.addControl(new mapboxgl.NavigationControl());
+  // Add geolocate control to the map.
+  map.addControl(new mapboxgl.GeolocateControl({
     positionOptions: {
-        enableHighAccuracy: true
+      enableHighAccuracy: true
     },
     trackUserLocation: true
-})
-);
+  })
+  );
 
 
-function punteros(){
-  var xmlhttp = new XMLHttpRequest();
-  var url = "dades.json"
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var datos = JSON.parse(xmlhttp.responseText);
-      
-      datos.forEach(function (marker){ // marker = datos[i]
-        // add markers to map
-        // create a HTML element for each feature
-        
-        var el = document.createElement('div');
-        el.className = 'marker-' + marker.tipus ;
+  function punteros() {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "dades.json"
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var datos = JSON.parse(xmlhttp.responseText);
 
-        // make a marker for each feature and add to the map
-        new mapboxgl.Marker(el)
-          .setLngLat([marker.geo1.long, marker.geo1.lat])
-          .setPopup(
-            new mapboxgl.Popup({ offset: 25 }) // add popups
-              .setHTML(
-                  '<h4>'  +
+        datos.forEach(function (marker) { // marker = datos[i]
+          // add markers to map
+          // create a HTML element for each feature
+
+          var el = document.createElement('div');
+          el.className = 'marker-' + marker.tipus;
+
+          // make a marker for each feature and add to the map
+          new mapboxgl.Marker(el)
+            .setLngLat([marker.geo1.long, marker.geo1.lat])
+            .setPopup(
+              new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML(
+                  '<h4>' +
                   marker.nom +
                   '</h4><p>' +
                   marker.geo1.address +
-                  '</p>'+
-                  '<img src="' + marker.imatges[0]+ '" style="height: 150px;"/>'  
-              )
-            )
-          .addTo(map);
-        });
-    }
-  };
-
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
-}
-
-punteros();
-
-
-       /*     marker = new mapboxgl.Marker()
-        .setLngLat([datos[i].geo1.long, datos[i].geo1.lat])
-        .setPopup(
-            new mapboxgl.Popup({ offset: 25 })
-                .setHTML(
-                    '<h4>'  +
-                    datos[i].nom +
-                    '</h4><p>' +
-                    datos[i].geo1.address +
-                    '</p>'+
-                    '<img src="' + datos[i].imatges[0]+ '" style="height: 150px;"/>'
-                    
+                  '</p>' +
+                  '<img src="' + marker.imatges[0] + '" style="height: 150px;"/>'
                 )
-        )
-        .addTo(map);*/
+            )
+            .addTo(map);
+        });
+      }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
+  punteros();
 
 
-function contador(){
-  var xmlhttp = new XMLHttpRequest();
-  var url = "dades.json"
-  var numSuper=0,numRest=0, numCursos=0,numInfo=0;
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+  /*     marker = new mapboxgl.Marker()
+   .setLngLat([datos[i].geo1.long, datos[i].geo1.lat])
+   .setPopup(
+       new mapboxgl.Popup({ offset: 25 })
+           .setHTML(
+               '<h4>'  +
+               datos[i].nom +
+               '</h4><p>' +
+               datos[i].geo1.address +
+               '</p>'+
+               '<img src="' + datos[i].imatges[0]+ '" style="height: 150px;"/>'
+               
+           )
+   )
+   .addTo(map);*/
+
+
+  function contador() {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "dades.json"
+    var numSuper = 0, numRest = 0, numCursos = 0, numInfo = 0;
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var datos = JSON.parse(xmlhttp.responseText);
-        var numSuper=0, numRest=0, numCursos=0, numInfo=0;
-        for(var i=0; i< datos.length; i++){
-          switch(datos[i].tipus){
+        var numSuper = 0, numRest = 0, numCursos = 0, numInfo = 0;
+        for (var i = 0; i < datos.length; i++) {
+          switch (datos[i].tipus) {
             case 'supermercat':
               numSuper++;
               break;
@@ -168,55 +243,55 @@ function contador(){
           }
         }
         //$("#numrest").append(document.createTextNode("Restaurants: "+ numRest));
-        $("#restText").append(document.createTextNode(numRest+" Restaurants"))
-        numRest= numRest*100
-        numRest= numRest+100
-        document.getElementById('rest').setAttribute("width", numRest+"px")
+        $("#restText").append(document.createTextNode(numRest + " Restaurants"))
+        numRest = numRest * 100
+        numRest = numRest + 100
+        document.getElementById('rest').setAttribute("width", numRest + "px")
         document.getElementById('rest').setAttribute("height", "35px")
-        
-       
-        numRest=numRest+5
-        document.getElementById('restText').setAttribute("x", numRest+"px");
-        
 
-        $("#superText").append(document.createTextNode(numSuper+" Supermercats"))
-        numSuper= numSuper*100;
-        numSuper= numSuper+100;
-        document.getElementById('super').setAttribute("width", numSuper+"px");
+
+        numRest = numRest + 5
+        document.getElementById('restText').setAttribute("x", numRest + "px");
+
+
+        $("#superText").append(document.createTextNode(numSuper + " Supermercats"))
+        numSuper = numSuper * 100;
+        numSuper = numSuper + 100;
+        document.getElementById('super').setAttribute("width", numSuper + "px");
         document.getElementById('super').setAttribute("height", "35px");
 
-        numSuper=numSuper+5
-        document.getElementById('superText').setAttribute("x", numSuper+"px");
+        numSuper = numSuper + 5
+        document.getElementById('superText').setAttribute("x", numSuper + "px");
 
 
-        $("#cursText").append(document.createTextNode(numCursos+" Cursos"))
-        numCursos= numCursos*100;
-        numCursos= numCursos+100;
-        document.getElementById('curs').setAttribute("width", numCursos+"px");
+        $("#cursText").append(document.createTextNode(numCursos + " Cursos"))
+        numCursos = numCursos * 100;
+        numCursos = numCursos + 100;
+        document.getElementById('curs').setAttribute("width", numCursos + "px");
         document.getElementById('curs').setAttribute("height", "35px");
 
-        numCursos=numCursos+5
-        document.getElementById('cursText').setAttribute("x", numCursos+"px");
+        numCursos = numCursos + 5
+        document.getElementById('cursText').setAttribute("x", numCursos + "px");
 
 
-        $("#infoText").append(document.createTextNode(numInfo+" + Informació"))
-        numInfo= numInfo*100;
-        numInfo= numInfo+100;
-        document.getElementById('mesinfo').setAttribute("width", numInfo+"px");
+        $("#infoText").append(document.createTextNode(numInfo + " + Informació"))
+        numInfo = numInfo * 100;
+        numInfo = numInfo + 100;
+        document.getElementById('mesinfo').setAttribute("width", numInfo + "px");
         document.getElementById('mesinfo').setAttribute("height", "35px");
 
-        numInfo=numInfo+5
-        document.getElementById('infoText').setAttribute("x", numInfo+"px");
-        
+        numInfo = numInfo + 5
+        document.getElementById('infoText').setAttribute("x", numInfo + "px");
+
 
         /*
          <rect id="numrest" width="180" height="35"></rect>
                         <text x="185" y="9.5" dy=".85em">8 restaurants</text>
         */
-    }
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
-}
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
 
-contador();
+  contador();
